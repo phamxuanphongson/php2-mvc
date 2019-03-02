da62ca4a7355f1a4c6fe1cedede84d8b7b19a6ad
phamxuanphongson <phamxuanphongson@gmail.com>
2019-03-02 22:03:25 +0700

    update db

--- php2.sql
+++ php2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 22, 2019 at 04:46 PM
+-- Generation Time: Mar 02, 2019 at 04:02 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -75,10 +75,25 @@ INSERT INTO `categories` (`id`, `name`, `color`) VALUES
 
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
-  `guestname` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `guestname` varchar(255) DEFAULT NULL,
   `comment` text NOT NULL,
   `posts_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `username`, `guestname`, `comment`, `posts_id`) VALUES
+(12, '', 'The thao', 'trtrtt', 48),
+(13, '', 'The thao22', '2222', 48),
+(14, '', 'Game22', '222222', 48),
+(15, '', 'Gamee', 'ddddd', 48),
+(16, 'phamxuanphongson', '', 'ddddddd', 46),
+(17, '', 'son', 'ok r', 48),
+(18, 'phamxuanphongson', '', 'ucucucuucc', 46),
+(19, 'phamxuanphongson', '', 'aaaa', 25);
 
 -- --------------------------------------------------------
 
@@ -102,28 +117,28 @@ CREATE TABLE `posts` (
 
 INSERT INTO `posts` (`id`, `title`, `short_desc`, `content`, `images`, `cate_id`, `view`) VALUES
 (21, 'Vĩnh Yên - Vĩnh Phúc - edited22', 'ddasd edited22', '2232331 edited', '43701026_1830278893755268_4186715432738095104_o.jpg', 3, 24),
-(22, 'aaaaaaaa post 30', '2333', '23313', 'Screenshot from 2019-02-17 11-03-57.png', 1, 4),
+(22, 'aaaaaaaa post 30', '2333', '23313', 'Screenshot from 2019-02-17 11-03-57.png', 1, 15),
 (23, 'ccccc', 'ccc21321313', 'casdasdasdasq3e2213', 'Screenshot from 2019-02-17 23-09-33.png', 1, 0),
 (24, 'post 123', '321213131321', 'ddddddddddddddddddddddddddddd', 'Screenshot from 2019-02-17 11-03-57.png', 1, 0),
-(25, 'Vũ Tông Phan', 'ddddddd', 'eeeeeeeeeeeeeee', 'Screenshot from 2019-02-17 11-03-57.png', 2, 1),
+(25, 'Vũ Tông Phan', 'ddddddd', 'eeeeeeeeeeeeeee', 'Screenshot from 2019-02-17 11-03-57.png', 2, 10),
 (26, 'khonssfsmksklfkmlf', 'dsdskjklajalmkasda', 'fmkemlmddsmklsdkmldklmdsf', 'Screenshot from 2019-02-17 23-09-33.png', 1, 0),
 (27, 'dad;lasl;kasl;asl;ssad', 'dadssadlalklddas', 'dadaklmamklsaklmadsklmadas', 'Screenshot from 2019-02-17 23-09-33.png', 4, 0),
 (28, 'dassssssssss', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'bbbbbbbbbbbbbbbbbbbbb', 'Screenshot from 2019-02-17 23-09-33.png', 1, 1),
 (29, 'ccccccccccccccccccccc', 'dddddddddddddddddddddddddddddddddd', 'eeeeeeeeeeeeeeeeeeeeeeeee', 'Screenshot from 2019-02-17 23-09-33.png', 3, 0),
-(30, 'hhhgghhg', 'fhggfhgffgd', 'ffdfdgdfdlkajdkaskjhakjakjdas', 'Screenshot from 2019-02-17 23-09-33.png', 1, 0),
+(30, 'hhhgghhg', 'fhggfhgffgd', 'ffdfdgdfdlkajdkaskjhakjakjdas', 'Screenshot from 2019-02-17 23-09-33.png', 1, 9),
 (31, 'aaaaaaaa post 3033', 'aaaaaaaa post 3033 aaaaaaaa post 3033 aaaaaaaa post 3033', 'aaaaaaaa post 3033aaaaaaaa post 3033aaaaaaaa post 3033aaaaaaaa post 3033aaaaaaaa post 3033aaaaaaaa post 3033', '43701026_1830278893755268_4186715432738095104_o.jpg', 2, 0),
 (32, 'jsiejsjksdfjdksdksldskj', 'dkmdskldskdlkldaskladsadlkklad', 'dknajandjdnsjakndasdansdamskaslmksamklsdmkl', '43701026_1830278893755268_4186715432738095104_o.jpg', 3, 0),
 (33, 'dadkmsdklsdkladkldajlkdsjidasdas', 'asddsamasdmladsmklsadmadsmdsa', 'refkjngjlkgtmgtkmklgtklmgrkmrlgt', '43701026_1830278893755268_4186715432738095104_o.jpg', 4, 0),
-(34, ' haf boi noif', 'fndsajsdjksdkmdlsaekmldskldssklddmsklsdmkl', 'sdfkmfsdkfsdmklfsdklmmklsfsdfklmsfkmlsfkml', 'Screenshot from 2019-02-17 23-09-33.png', 1, 0),
+(34, ' haf boi noif', 'fndsajsdjksdkmdlsaekmldskldssklddmsklsdmkl', 'sdfkmfsdkfsdmklfsdklmmklsfsdfklmsfkmlsfkml', 'Screenshot from 2019-02-17 23-09-33.png', 1, 5),
 (35, ' Vũ Tông PhanVũ Tông Phan', 'Vũ Tông PhanVũ Tông PhanVũ Tông PhanVũ Tông Phan', 'Vũ Tông PhanVũ Tông PhanVũ Tông PhanVũ Tông Phan', 'Screenshot from 2019-02-17 23-09-33.png', 3, 0),
-(36, 'dkjsskdsdkmljsdksdkljdjsa', 'dsadsdjjdksakldaskladdkajsdkls', 'adkdksdjskdkdajkdsajkda', 'Screenshot from 2019-02-17 23-09-33.png', 1, 0),
+(36, 'dkjsskdsdkmljsdksdkljdjsa', 'dsadsdjjdksakldaskladdkajsdkls', 'adkdksdjskdkdajkdsajkda', 'Screenshot from 2019-02-17 23-09-33.png', 1, 2),
 (41, 'ddaadsa', 'aaasddasdasd', 'dadaasdasasdasasasssad', 'Screenshot from 2019-02-17 23-09-33.png', 1, 0),
 (42, 'asasdjasdkasajskdjsakadksljkldaskljsdakljsadjklssddda', 'asasdjasdkasajskdjsakadksljkldaskljsdakljsadjklsdaasasdjasdkasajskdjsakadksljkldaskljsdakljsadjklsda', 'asasdjasdkasajskdjsakadksljkldaskljsdakljsadjklsdaasasdjasdkasajskdjsakadksljkldaskljsdakljsadjklsdaasasdjasdkasajskdjsakadksljkldaskljsdakljsadjklsdaasasdjasdkasajskdjsakadksljkldaskljsdakljsadjklsda', 'Screenshot from 2019-02-17 23-09-33.png', 1, 0),
 (43, 'kudoadoiiqwdoiqqkowppoqw', 'kudoadoiiqwdoiqqkowppoqwkudoadoiiqwdoiqqkowppoqw', 'kudoadoiiqwdoiqqkowppoqwkudoadoiiqwdoiqqkowppoqwkudoadoiiqwdoiqqkowppoqwkudoadoiiqwdoiqqkowppoqw', 'Screenshot from 2019-02-17 11-03-57.png', 2, 0),
 (44, 'adadddadasdsass', 'kudoadoiiqwdoiqqkowppoqwkudoadoiiqwdoiqqkowppoqwkudoadoiiqwdoiqqkowppoqw', 'kudoadoiiqwdoiqqkowppoqwkudoadoiiqwdoiqqkowppoqwkudoadoiiqwdoiqqkowppoqwkudoadoiiqwdoiqqkowppoqw', 'Screenshot from 2019-02-17 23-09-33.png', 2, 0),
-(45, 'dadsdaasdasas', 'dadsdaasdasasdadsdaasdasasdadsdaasdasasdadsdaasdasas', 'dadsdaasdasasdadsdaasdasasdadsdaasdasasdadsdaasdasasdadsdaasdasas', 'Screenshot from 2019-02-17 11-03-57.png', 3, 0),
-(46, 'dadsdaasdasasdadsdaasdasas', 'dadsdaasdasasdadsdaasdasasdadsdaasdasasdadsdaasdasas', 'dadsdaasdasasdadsdaasdasas', '43701026_1830278893755268_4186715432738095104_o.jpg', 2, 1),
-(48, 'aaaaaaa', 'aaaaa', 'aaaaaa', '43701026_1830278893755268_4186715432738095104_o.jpg', 2, 9);
+(45, 'dadsdaasdasas', 'dadsdaasdasasdadsdaasdasasdadsdaasdasasdadsdaasdasas', 'dadsdaasdasasdadsdaasdasasdadsdaasdasasdadsdaasdasasdadsdaasdasas', 'Screenshot from 2019-02-17 11-03-57.png', 3, 1),
+(46, 'dadsdaasdasasdadsdaasdasas', 'dadsdaasdasasdadsdaasdasasdadsdaasdasasdadsdaasdasas', 'dadsdaasdasasdadsdaasdasas', '43701026_1830278893755268_4186715432738095104_o.jpg', 2, 22),
+(48, 'aaaaaaa', 'aaaaa', 'aaaaaa', '43701026_1830278893755268_4186715432738095104_o.jpg', 2, 58);
 
 -- --------------------------------------------------------
 
@@ -136,7 +151,7 @@ CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `role` int(11) NOT NULL DEFAULT '0'
+  `role` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -144,12 +159,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `role`) VALUES
-(28, 'phamxuanphongson', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'phamxuanphongson@gmail.com', 1),
-(29, 'admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'admin@gmail.com', 0),
-(40, 'phamxuanphongson', '894b54b6fb953a6003d4112aa2c9a8d0096c64b6', '', 0),
-(41, 'phamxuanphongson', '7c86dc874e0863a64b9e49448cd3218c3c0efce1', 'admi1n@gmail.com', 0),
-(43, 'phamxuanphongson', '4ec78cb94fc7612597f3f02d46b29ba03481208c', 'phamxuanphongs122on@gmail.com', 0),
-(44, 'mai', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'mai@gmail.com', 0);
+(52, 'phamxuanphongson', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'phamxuanphongson@gmail.com', 1);
 
 --
 -- Indexes for dumped tables
@@ -206,7 +216,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `posts`
@@ -218,7 +228,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
